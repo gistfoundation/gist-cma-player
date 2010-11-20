de2e971ddc33bdff3b0504ed7cf3f876b313abd0
Ian Ibbotson <ianibbo@gmail.com>
2010-11-20 15:17:42 +0000

    Some work on ui

---
+++ backend/cma/grails-app/services/uk/org/commedia/epg/FeedSyncService.groovy
@@ -0,0 +1,43 @@
+package uk.org.commedia.epg
+
+import grails.converters.*
+
+class FeedSyncService {
+
+    static transactional = false
+
+    def doSync() {
+      long timestamp = System.currentTimeMillis()
+      println "Loading live feeds from commedia ice cast rss feed"
+      def live_feed_url = "http://icecast.commedia.org.uk/rss.xml"
+      def rss_response_text = live_feed_url.toURL().text
+      println "Rss response: ${rss_response_text}"
+      def rss = new XmlSlurper().parseText(rss_response_text)
+      println "Rss: ${rss}"
+      rss.channel.item.each {
+        try {
+          def guid = it.guid?.text()
+          def title = it.title?.text()
+          def description = it.description?.text()
+          def link = it.link?.text()
+
+          println "Processing station guid:${guid} link:${link} desc:${description} title:${title}"
+
+          def station = Station.findByGuid(guid) ?: new Station(name:title,description:description,guid:guid,url:link,source:"CMRSS")
+          station.live = true;
+          station.lastSeen = timestamp
+          if ( station.save() ) {
+            println "${station.name} (${station.guid}) - live"
+          }
+          else {
+            println station.errors.allErrors.each {
+               println it.defaultMessage
+            }
+          }
+        }
+        catch(Exception e) {
+          println "erorr ${e}"
+        }
+      }
+    }
+}
