96596b9be6a6db551b4dcb6edccf5fc7d5769730
Ian Ibbotson <ianibbo@gmail.com>
2010-11-20 11:05:54 +0000

    Added phoneapp dir, cleaned up backed, which now processes canstream rss feed

--- backend/cma/grails-app/conf/BootStrap.groovy
+++ backend/cma/grails-app/conf/BootStrap.groovy
@@ -4,6 +4,7 @@ class BootStrap {
 
   // These two beans will be auto injected from the spring application context based on the class names. See the grails-app/services dir for the vocab sync service
   def springSecurityService
+  def feedSyncService
 
   def init = { servletContext ->
     def user_role = Role.findByAuthority('user') ?: new Role(authority: 'user').save()
@@ -12,9 +13,12 @@ class BootStrap {
     def admin_user = User.findByUsername('admin') ?: new User(username: 'admin', password: springSecurityService.encodePassword('admin'), enabled:true).save()
     def cma_user = User.findByUsername('cma') ?: new User(username: 'cma', password: springSecurityService.encodePassword('cma'), enabled:true).save()
 
-    def working_station = Station.findByName('TheEyeFM') ?: new Station(name:'TheEyeFM',url:'http://icecast.commedia.org.uk:8000/theeyefm.mp3').save()
+    def working_station = Station.findByGuid('TheEyeFM') ?: new Station(name:'TheEyeFM',url:'http://icecast.commedia.org.uk:8000/theeyefm.mp3',guid:'TheEyeFM').save()
 
     println "Station: ${working_station}"
+
+    println "Calling feed sync service"
+    feedSyncService.doSync()
   }
   def destroy = {
   }
--- backend/cma/grails-app/domain/uk/org/commedia/epg/Station.groovy
+++ backend/cma/grails-app/domain/uk/org/commedia/epg/Station.groovy
@@ -2,9 +2,22 @@ package uk.org.commedia.epg
 
 class Station {
 
-  static constraints = {
-  }
-
   String name
   String url
+  String guid
+  String description
+  Boolean live=false
+  // Where we got the info from - So we can detect stations not broadcasting
+  String source
+  // When did we last see this station
+  long lastSeen
+
+  static constraints = {
+    name(nullable:false)
+    url(nullable:false)
+    guid(blank:false,nullable:false)
+    description(size:0..4000,blank:true,nullable:true)
+    live(nullable:false)
+    source(nullable:true)
+  }
 }
