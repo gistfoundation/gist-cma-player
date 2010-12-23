0615f5d7c841ee4896f89bb2fd01a84d44c50736
dev <dev@richs-imac.local>
2010-12-23 15:20:48 +0000

    Intial attempt at adding ios project

---
+++ phoneapp/ios/Classes/CanstreamerAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  CanstreamerAppDelegate.h
+//  Canstreamer
+//
+//  Created by dev on 23/12/2010.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PhoneGapDelegate.h"
+
+@interface CanstreamerAppDelegate : PhoneGapDelegate {
+}
+
+@end
+
---
+++ phoneapp/ios/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Canstreamer
+//
+//  Created by dev on 23/12/2010.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"CanstreamerAppDelegate");
+    [pool release];
+    return retVal;
+}
